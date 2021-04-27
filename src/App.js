f473699a152e6f1861931e80db953093f7b16afe
lav senghani <lav.relytree@gmail.com>
2021-04-27 11:26:32 +0530

    changed HOC to handle description block

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ParentContainer from "./component";
 
-const App = ({ text, columnSchema }) => {
-  console.log(columnSchema, "columnSchema");
-  return <div className="App">{text}</div>;
+const App = ({ blockData }) => {
+  console.log(blockData, "blockData");
+  return <div className="App">{blockData.type + "block"}</div>;
 };
 
-export default ParentContainer(App, "blk0hFSH9tPcltVxu3");
+export default ParentContainer(App, "blk0hFSH9tPcltVxu2");
--- src/component/index.js
+++ src/component/index.js
@@ -11,29 +11,43 @@ const withWidgets = (WrappedComponent, blockID) => {
         columnSchema: null,
         rowData: null,
         loading: true,
+        loadDescriptionBlock: true,
       };
     }
 
     componentDidMount() {
+      // dashboardtables / blk0hFSH9tPcltVxu3;
       fetch(`https://dev9.stackby.com/api/v1/blockdetails/${blockID}`)
         .then((response) => response.json())
         .then((data) => {
           data.data.blockFields = JSON.parse(data.data.blockFields);
-          this.setState({
-            blockData: data.data,
-            columnSchema: data.data.blockFields.rowCardData.columnSchema
-              ? data.data.blockFields.rowCardData.columnSchema
-              : null,
-            rowData: data.data.blockFields.rowCardData.rowData
-              ? data.data.blockFields.rowCardData.rowData
-              : null,
-            loading: false,
-          });
+          if (data.data.type === "description") {
+            this.setState({
+              blockData: data.data,
+              loadDescriptionBlock: false,
+            });
+          } else {
+            // this.setState({
+            //   blockData: data.data,
+            // });
+            // fetch(`https://dev9.stackby.com/api/v1/dashboardtables/${blockID}`)
+            // .then((response) => response.json())
+            // .then((data) => {
+            // });
+          }
         });
     }
     render() {
-      console.log("log from withWidgets");
-      if (!this.state.loading) {
+      if (!this.state.loadDescriptionBlock) {
+        return (
+          <div>
+            <WrappedComponent
+              {...this.props}
+              blockData={this.state.blockData}
+            />
+          </div>
+        );
+      } else if (!this.state.loading) {
         return (
           <div>
             <WrappedComponent
